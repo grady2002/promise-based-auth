a34f282e7b93871ea4c7705820ad34f32431b144
Aatif Mastan <aatifmastan644@gmail.com>
2022-03-15 10:49:45 +0530

    initial commit

---
+++ auth.ts
@@ -0,0 +1,37 @@
+console.clear();
+
+const users = () => {
+  let u = [
+    { username: "admin", password: "admin" },
+    { username: "grady", password: "grady" },
+    { username: "user", password: "user" },
+  ];
+  return u;
+};
+
+const auth = new Promise((resolve: Function, reject: Function) => {
+  const authWith = (username: string = "null", password: string = "null") => {
+    let bool = users().find((user: any) => {
+      if (username == user.username && password == user.password) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return bool;
+  };
+  let oAuth = authWith("admin", "admin");
+  if (!oAuth) {
+    reject("Incorrect username or password");
+  } else {
+    resolve(`Logged in as ${oAuth.username}`);
+  }
+});
+
+auth
+  .then((result: any) => {
+    console.log(result);
+  })
+  .catch((err: string) => {
+    if (err) console.error(err);
+  });
